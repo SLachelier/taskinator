9ce875ccc81b40dd7344faa9ac1d7f37c019f9f0
Shauna L <slachelier@gmail.com>
2022-06-08 17:33:58 -0500

    added added more functionality for localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,8 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var pageContentEl = document.querySelector("#page-content");
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
+var tasks = [];
+
 
 var taskButtonHandler = function(event) {
     //gets the target element from the event
@@ -44,6 +46,16 @@ var editTask = function(taskId) {
 var deleteTask = function(taskId) {
     var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
     taskSelected.remove();
+
+    var updatedTaskArr = []; //array to hold updated list of tasks
+    for(var i = 0; i < tasks.length; i++) {
+        //if tasks[i].id doesn't match the value of taskId, lets keep that task and push it into the new array
+        if(tasks[i].id !== parseInt(taskId)) {
+            updatedTaskArr.push(tasks[i]);
+        }
+    }
+    //reassigns tasks array to be the same as updatedTaskArr
+    tasks = updatedTaskArr;
 };
 
 var completeEditTask = function(taskName, taskType, taskId) {
@@ -53,6 +65,14 @@ var completeEditTask = function(taskName, taskType, taskId) {
     //set new values
     taskSelected.querySelector("h3.task-name").textContent = taskName;
     taskSelected.querySelector("span.task-type").textContent = taskType;
+
+    //loop through tasks array and task object with new content
+    for(var i = 0; i < tasks.length; i++) {
+        if(tasks[i].id === parseInt(taskId)) {
+            tasks[i].name = taskName;
+            tasks[i].type = taskType;
+        }
+    };
 
     alert("Task updated!");
     formEl.removeAttribute("data-task-id");
@@ -82,7 +102,8 @@ var taskFormHandler = function(event) {
     //package up data as an object
         var taskDataObj = {
             name: taskNameInput,
-            type: taskTypeInput
+            type: taskTypeInput,
+            status: "to do"
         };
     }
 
@@ -108,12 +129,16 @@ var createTaskEl = function(taskDataObj) {
 
     var taskActionsEl = createTaskActions(taskIdCounter);
     listItemEl.appendChild(taskActionsEl);
-
     //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
 
+    taskDataObj.id = taskIdCounter;
+    tasks.push(taskDataObj);
+
     //increase task counter for next unique id
     taskIdCounter++;
+    console.log(taskDataObj);
+    console.log(taskDataObj.status);
 };
 
 var createTaskActions = function(taskId) {
@@ -155,6 +180,7 @@ var createTaskActions = function(taskId) {
     actionContainerEl.appendChild(statusSelectEl);
 
     return actionContainerEl;
+
 };
 
 var taskStatusChangeHandler = function(event) {
@@ -172,6 +198,13 @@ var taskStatusChangeHandler = function(event) {
     } else if(statusValue === "completed") {
         tasksCompletedEl.appendChild(taskSelected);
     }
+
+    for(var i = 0; i < tasks.length; i++) {
+        if(tasks[i].id === parseInt(taskId)) {
+            tasks[i].status = statusValue;
+        }
+    }
+    console.log(tasks);
 };
 
 formEl.addEventListener("submit", taskFormHandler);
